6203d36b221a3aa6afce98f4cb26ff1ab6a9755e
Ferid547 <igidov.ferid1@gmail.com>
2024-01-04 14:09:36 +0400

    Ingress

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from "./pages/About";
 function App() {
   return (
     <div className="App">
-      Hello Ingress Academy
+      Hello Ingress Academy dsuhfudshu
       <Routes>
         <Route path="/"  element={<Home/>}></Route>
         <Route path="/about"  element={<About/>}></Route>
